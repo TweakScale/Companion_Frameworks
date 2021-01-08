3089736bea9bcb2e594ccd6d6a289cfb4fc45f83
Lisias <me@lisias.net>
2021-01-08 19:08:22 -0300

    Incepting the Companion statup DLL.

---
+++ Source/TweakScaleCompanion_EyeCandies/Startup.cs
@@ -0,0 +1,62 @@
+﻿/*
+	This file is part of TweakScaleCompanion_EyeCandies
+	© 2021 LisiasT : http://lisias.net <support@lisias.net>
+
+	TweakScaleCompanion_EyeCandies is double licensed, as follows:
+
+	* SKL 1.0 : https://ksp.lisias.net/SKL-1_0.txt
+	* GPL 2.0 : https://www.gnu.org/licenses/gpl-2.0.txt
+
+	And you are allowed to choose the License that better suit your needs.
+
+	TweakScaleCompanion_EyeCandies is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+	You should have received a copy of the SKL Standard License 1.0
+	along with TweakScaleCompanion_EyeCandies. If not, see <https://ksp.lisias.net/SKL-1_0.txt>.
+
+	You should have received a copy of the GNU General Public License 2.0
+	along with TweakScaleCompanion_EyeCandies. If not, see <https://www.gnu.org/licenses/>.
+
+*/
+using System;
+using UnityEngine;
+
+namespace TweakScaleCompanion.EyeCandies
+{
+	[KSPAddon(KSPAddon.Startup.Instantly, true)]
+	internal class Startup : MonoBehaviour
+	{
+		private void Start()
+		{
+			Log.init();
+			Log.force("Version {0} Alpha", Version.Text);
+
+			try
+			{
+				KSPe.Util.Installation.Check<Startup>("TweakScaleCompanion", "EyeCandies", null);
+			}
+			catch (KSPe.Util.InstallmentException e)
+			{
+				Log.error(e.ToShortMessage());
+				KSPe.Common.Dialogs.ShowStopperAlertBox.Show(e);
+			}
+
+			this.checkDependencies();
+		}
+
+		private void checkDependencies()
+		{
+			// Linq is giving me "Exception System.InvalidOperationException: Operation is not valid due to the current state of the object".
+			// So I got rid of that crap.
+			foreach (AssemblyLoader.LoadedAssembly assembly in AssemblyLoader.loadedAssemblies) if ("Scale" == assembly.assembly.GetName().Name)
+			{
+				Log.detail("Found {0}", assembly.assembly.FullName);
+				if (assembly.assembly.GetName().Version.CompareTo(new System.Version(2, 4, 4)) >= 0) return;
+				break;
+			}
+			GUI.UnmetRequirementsShowStopperAlertBox.Show("TweakScale v2.4.4 or superior");
+		}
+	}
+}
