67febf335641b6370aa060b69a6be1eea0c6944b
Lisias T <me@lisias.net>
2021-08-08 08:23:28 -0300

    Fixes https://github.com/net-lisias-ksp/TweakScaleCompanion_Visuals/issues/1 (HOLLY CRAP!!)

--- Source/TweakScalerWaterfallFX/Properties/AssemblyInfo.cs
+++ Source/TweakScalerWaterfallFX/Properties/AssemblyInfo.cs
@@ -23,6 +23,6 @@ using System.Runtime.CompilerServices;
 [assembly: KSPAssembly("TweakScalerModuleWaterfallFX", TweakScaleCompanion.Visuals.Version.major, TweakScaleCompanion.Visuals.Version.minor)]
 [assembly: KSPAssemblyDependency("KSPe.Light.TweakScale", 2, 1)]
 [assembly: KSPAssemblyDependency("Scale", 2, 4)]
-[assembly: KSPAssemblyDependency("Scale_Redist", 1, 0)]
+//[assembly: KSPAssemblyDependency("Scale_Redist", 1, 0)] // KSP 1.12.2 screwed up the Dependenct check!!!
 [assembly: KSPAssemblyDependency("TweakScaleCompanion_Visuals", TweakScaleCompanion.Visuals.Version.major, TweakScaleCompanion.Visuals.Version.minor)]
 [assembly: KSPAssemblyDependency("Waterfall", 0, 0)]
