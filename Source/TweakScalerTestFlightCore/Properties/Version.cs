0f65d15dae1d6f1ab315fa83646333ca1f6b7ebb
Lisias T <me@lisias.net>
2023-04-16 05:49:16 -0300

    WHOOPS!! Late commit of missing files (that would be going to hurt!!)

---
+++ Source/TweakScalerTestFlightCore/Properties/Version.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace TweakScaleCompanion.Frameworks.TestFlightCore
+{
+	public static class Version
+	{
+		public const int major = Frameworks.Version.major;
+		public const int minor = Frameworks.Version.minor;
+		public const int patch = Frameworks.Version.patch;
+		public const int build = Frameworks.Version.build;
+		public const string Number = Frameworks.Version.Number;
+		public const string Text = Frameworks.Version.Text;
+		public const bool NamespaceAsDirectories = Frameworks.Version.NamespaceAsDirectories;
+	}
+}
